f8e38a771e176577a9e74252a1b8733fb3cedfa4
Camden Harris <camdenharris@gmail.com>
2024-06-07 00:05:03 -0700

    push so victor can try doing requests since i got bonked by spotify API

--- spotify_scraping.py
+++ spotify_scraping.py
@@ -70,6 +70,17 @@ def get_album_songs(token, album_id):
     return song_names
 
 
+def get_artist_top_songs(token, artist_id):
+    url = (
+        f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
+    )
+    headers = get_auth_header(token)
+    result = requests.get(url, headers=headers)
+    print(result)
+    json_result = json.loads(result.content)["tracks"]
+    return json_result
+
+
 def get_all_songs_by_artist(token, artist_id) -> list:
     return [
         song
@@ -78,18 +89,87 @@ def get_all_songs_by_artist(token, artist_id) -> list:
     ]
 
 
+def get_track_features(token, track_id) -> dict:
+    url = f"https://api.spotify.com/v1/audio-features/{track_id}"
+    headers = get_auth_header(token)
+    result = requests.get(url, headers=headers)
+    json_result: dict = json.loads(result.content)
+    json_result.pop("uri")
+    json_result.pop("track_href")
+    json_result.pop("analysis_url")
+
+    return json_result
+
+
+def get_track_analysis(token, track_id) -> dict:
+    url = f"https://api.spotify.com/v1/audio-analysis/{track_id}"
+    headers = get_auth_header(token)
+    result = requests.get(url, headers=headers)
+    json_result: dict = json.loads(result.content)
+
+    important_data: dict = json_result["track"]
+
+    drop_keys = [
+        "duration",
+        "loudness",
+        "tempo",
+        "num_samples",
+        "sample_md5",
+        "offset_seconds",
+        "window_seconds",
+        "analysis_sample_rate",
+        "analysis_channels",
+        "end_of_fade_in",
+        "start_of_fade_out",
+        "codestring",
+        "code_version",
+        "echoprintstring",
+        "echoprint_version",
+        "synchstring",
+        "synch_version",
+        "rhythmstring",
+        "rhythm_version",
+    ]
+
+    for key in drop_keys:
+        important_data.pop(key)
+
+    return important_data
+
+
 def main():
     token = get_token()
 
-    with open("artists.csv") as f:
-        all_songs = []
-        for artist in f:
-            artist_id = search_for_artist(token, artist.strip())["id"]
-            for song in get_all_songs_by_artist(token, artist_id):
-                all_songs.append(song)
-            print(f"{artist.strip()} completed")
-        df = pd.DataFrame(all_songs)
-        df.to_csv("songs.csv", index=False)
+    # with open("artists.csv") as f:
+    #     all_songs = []
+    #     for artist in f:
+    #         artist_id = search_for_artist(token, artist.strip())["id"]
+    #         print(artist_id)
+    #         print(get_artist_top_songs(token, artist_id))
+    #         # print(f"{artist.strip()} completed")
+    #     df = pd.DataFrame(all_songs)
+    #     df.to_csv("songs.csv", index=False)
+
+    # artist_name = "Rihanna"
+    # artist_id = search_for_artist(token, artist_name)["id"]
+    # print(f"{artist_name=}")
+    # print(f"{artist_id=}")
+    print(f"{get_artist_albums(token, "1Xyo4u8uXC1ZmMpatF05PJ")=}")
+
+    # with open("songs.csv") as f:
+    #     output = []
+    #     for idx, song in enumerate(f):
+    #         track_info = get_track_features(token, song.strip())
+    #         track_info.update(get_track_analysis(token, song.strip()))
+    #         output.append(track_info)
+
+    #         print(f"{song} completed")
+
+    #         if idx == 10:
+    #             break
+
+    #     df = pd.DataFrame(output)
+    #     df.to_csv("song_data.csv", index=False)
 
 
 if __name__ == "__main__":
